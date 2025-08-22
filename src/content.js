c330e854d452fbf07d6f4bd9c01cc154545a5df9
George Ibrahim Aziz <103423847+georgeibrahim1@users.noreply.github.com>
2025-08-22 03:19:57 +0300

    feat: sort the tabs by thier lastAccessTime

--- src/content.js
+++ src/content.js
@@ -1,3 +1,16 @@
 (function() {
-  console.log("Content script injected via Alt+Q!");
+    console.log("Content script injected via Alt+Q!");
+
+  
+    chrome.runtime.sendMessage({ action: "getTabs" }, (response) => {
+        console.log(response);
+
+        response.sort((a,b)=>{
+            return b.lastAccessed - a.lastAccessed;
+        });
+
+        console.log(response);
+
+    });   
+
 })();
